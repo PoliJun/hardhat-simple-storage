2fd8f4fbcbf98ccfe2235db98092c88bb418c055
Huntley Murdock <rivenloving@gmail.com>
2023-04-27 01:45:20 +0800

    added verify function, added hardhat plugin:etherscan,aquired etherscan api key

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,28 @@
-require("@nomicfoundation/hardhat-toolbox");
-
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 module.exports = {
-  solidity: "0.8.18",
-};
+    // default network is hardhat network
+    // you can use: --network to specify the network when run deploy.js
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY], // this should be accounts rather than account!
+            chainId: 5,
+        },
+    },
+    solidity: "0.8.18",
+    etherscan: {
+        apiKey: {// this is apiKey rather than apikey, bugged here
+            goerli: ETHERSCAN_API_KEY,
+        },
+    },
+}
+
+// there is a lot of plugins on hardhat
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,63 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat")
+// imports
+const { error } = require("console")
+const { ethers, run, network } = require("hardhat")
+require("dotenv").config()
 
+//async main
 async function main() {
-    const currentTimestampInSeconds = Math.round(Date.now() / 1000)
-    const unlockTime = currentTimestampInSeconds + 60
-
-    const lockedAmount = hre.ethers.utils.parseEther("0.001")
-
-    const Lock = await hre.ethers.getContractFactory("Lock")
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount })
-
-    await lock.deployed()
-
-    console.log(
-        `Lock with ${ethers.utils.formatEther(
-            lockedAmount
-        )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
     )
+    console.log("Deploying Contract")
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    await simpleStorage.deployed()
+    // where is the private key
+    // where is the rpc url
+    console.log(`Deployed contract ${simpleStorage.address}`)
+    // what happens when we deploy the contract to hardhat network
+    // console.log(network.config)
+
+    // 4==4 -> true
+    // 4=="4"->true
+    // 4==="4"->false
+
+    // if process.env.ETHERSCAN_API_KEY exists then it's true, otherwise it's false
+    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deployTransaction.wait(6) // wait for at most 6 blocks in case of waiting for blocks behind to complete
+        await verify(simpleStorage.address, [])
+    }
+
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`current value is: ${currentValue}`)
+
+    // Update the current value
+    const transactionResponse = await simpleStorage.store(7)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated value is: ${updatedValue}`)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-    console.error(error)
-    process.exitCode = 1
-})
+// verify
+async function verify(contractAddress, args) {
+    console.log("Verifying contract ... ")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified!")
+        } else {
+            console.log(e)
+        }
+    }
+}
+
+// main
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
